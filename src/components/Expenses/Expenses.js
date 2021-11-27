1e1fcd78ef1d8665f0a0741b29dc1b9e59abec5a
camcaro <carlos.camara.ortiz@gmail.com>
2021-11-27 09:08:07 +0100

    feat: add basic components to show expenses list

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,22 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expenses = props.expenses;
+  // console.log(props);
+  return (
+    <Card className="expenses">
+      {expenses.map(expense => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
